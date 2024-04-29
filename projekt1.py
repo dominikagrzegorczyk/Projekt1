f61dbd861a652f432252724fab2f63a2ca110cec
wika1122 <2002wika@wp.pl>
2024-04-29 09:29:15 +0200

    dodano definicję funkcji xyz2neup

--- projekt1.py
+++ projekt1.py
@@ -119,3 +119,67 @@ class Transformacje:
             e = dx[1]
             up = dx[2]
             return f"{n:.3f}", f"{e:.20f}", f"{up:.3f}"
+
+        def fl_do_2000(self, lat, lon):
+            '''
+            Transformacja współrzęnych geodezyjnych szerokosci (phi) i długosci (lam) na współrzędne układu związane z systemem 2000.
+            Poprzez odwzorowanie czterostefowe Gaussa-Krugera w pasie 3-stopniowym przy południkach osiowych (15°, 18°, 21°, 24°)
+            ponumerowanych odpowiednio: 5, 6, 7 i 8 oraz przy współczynniku skali równym 0.999923.
+
+            Parameters
+            ----------
+            Parameters:
+           lat : float
+              [stopnie] -  szerokość geodezyjna.
+           lon : float
+              [stopnie] -  długość geodezyjna.
+
+
+            Returns
+            -------
+            x : float
+                Współrzędna X w układzie związanym z systemem 2000.
+            y : float
+                Współrzędna Y w układzie związanym z systemem 2000.
+            z : float
+                Współrzędna Z w układzie związanym z systemem 2000.
+
+            '''
+            if lon >= 13.5 and lon < 16.5:
+                lon0 = 15
+                nr = 5
+            elif lon >= 16.5 and lon < 19.5:
+                lon = 18
+                nr = 6
+            elif lon >= 19.5 and lon < 22.5:
+                lon0 = 21
+                nr = 7
+            elif lon >= 22.5 and lon <= 25.5:
+                lon0 = 24
+                nr = 8
+
+            lat = radians(lat)
+            lon = radians(lon)
+            lon0 = radians(lon0)
+
+            b2 = self.a ** 2 * (1 - self.ecc2)
+            e2_poch = (self.a ** 2 - b2) / b2
+            delta_l = lon - lon0
+            t = tan(lat)
+            n2 = e2_poch * np.cos(lat) ** 2
+            N = self.a / sqrt(1 - self.ecc2 * sin(lat) ** 2)
+            A0 = 1 - self.ecc2 / 4 - 3 * self.ecc2 ** 2 / 64 - 5 * self.ecc2 ** 3 / 256
+            A2 = (3 / 8) * (self.ecc2 + self.ecc2 ** 2 / 4 + 15 * self.ecc2 ** 3 / 128)
+            A4 = (15 / 256) * (self.ecc2 ** 2 + 3 * self.ecc2 ** 3 / 4)
+            A6 = (35 * self.ecc2 ** 3) / 3072
+            si = self.a * (A0 * lat - A2 * np.sin(2 * lat) + A4 * np.sin(4 * lat) - A6 * np.sin(6 * lat))
+            xgk = si + (delta_l ** 2 / 2) * N * np.sin(lat) * np.cos(lat) * (
+                        1 + (delta_l ** 2 / 12) * np.cos(lat) ** 2 * (5 - t ** 2 + 9 * n2 + 4 * n2 ** 2) + (
+                            delta_l ** 4 / 360) * np.cos(lat) ** 4 * (61 - 58 * t ** 2 + 270 * n2 - 330 * n2 * t ** 2))
+            ygk = delta_l * N * np.cos(lat) * (
+                        1 + (delta_l ** 2 / 6) * np.cos(lat) ** 2 * (1 - t ** 2 + n2) + (delta_l ** 4 / 120) * np.cos(
+                    lat) ** 4 * (5 - 18 * t ** 2 + t ** 4 + 14 * n2 - 58 * n2 * t ** 2))
+            mo = 0.999923
+            xgk_2000 = xgk * mo
+            ygk_2000 = ygk * mo + nr * 1000000 + 500000
+            return f"{xgk_2000:.3f}", f"{ygk_2000:.3f}"
