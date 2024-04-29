1de9b83a4dfb8bd93bb4638f97ace60c56de2893
wika1122 <2002wika@wp.pl>
2024-04-29 09:32:06 +0200

    zmodyfikowano definicję funkcji fl_do_2000

--- projekt1.py
+++ projekt1.py
@@ -126,7 +126,7 @@ class Transformacje:
             Poprzez odwzorowanie czterostefowe Gaussa-Krugera w pasie 3-stopniowym przy południkach osiowych (15°, 18°, 21°, 24°)
             ponumerowanych odpowiednio: 5, 6, 7 i 8 oraz przy współczynniku skali równym 0.999923.
 
-            ----------
+            ---------
             Parameters:
            lat : float
               [stopnie] -  szerokość geodezyjna.
